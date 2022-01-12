8c38e2cb4d615731828d099ec599b8790ebd9e75
Jillian Anderson <willfrog1224@yahoo.com>
2022-01-12 15:32:53 -0800

    update

--- week2/app.js
+++ week2/app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -7,9 +9,14 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
 app.listen(3000);
 
--- week2/routes/admin.js
+++ week2/routes/admin.js
@@ -1,16 +1,20 @@
+const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.send(
-        '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-router.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-    
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 module.exports = router;
