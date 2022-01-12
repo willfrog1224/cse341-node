3a3a4b0c0eca9f20347c885a76faf7995c542821
Jillian Anderson <willfrog1224@yahoo.com>
2022-01-12 13:06:41 -0800

    update

--- week2/app.js
+++ week2/app.js
@@ -1,26 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-// app.use((req, res, next) => {
-//     console.log('In the middleware');
-//     next();
-// });
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/', (req, res, next) => {
-    console.log('This always runs');
-    next();
-}); 
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-    console.log('In another middleware');
-    res.send('<h1>Hello from Express!</h1>');
-});
-
-app.use('/', (req, res, next) => {
-    console.log('In another middleware');
-    res.send('<h1>Hello from Express!</h1>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
 
