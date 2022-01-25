ce9f40489e37edabeeb52b1a309d72edd0b1f197
Jillian Anderson <willfrog1224@yahoo.com>
2022-01-25 10:12:52 -0800

    u

--- week2/util/database.js
+++ week2/util/database.js
@@ -5,7 +5,7 @@ let _db;
 
 const mongoConnect = callback => {
   MongoClient.connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+    'mongodb+srv://jill:mongoUser1@cluster0.ouhbd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
   )
     .then(client => {
       console.log('Connected!');
