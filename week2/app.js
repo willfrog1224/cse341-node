e3cd1c51c6b984d2b798d451003a5c05aa6830cd
Jillian Anderson <willfrog1224@yahoo.com>
2022-01-27 15:46:46 -0800

    update

--- week2/app.js
+++ week2/app.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const PORT = process.env.PORT || 3000;
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -19,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
+  User.findById('61f316322f8d54c2523099c5')
     .then(user => {
       req.user = user;
       next();
@@ -32,10 +33,8 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoose
-  .connect(
-    'mongodb+srv://jill:mongoUser1@cluster0.ouhbd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-  )
+mongoose.connect('mongodb+srv://jill:mongoUser1@cluster0.ouhbd.mongodb.net/shop?retryWrites=true')
+
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
